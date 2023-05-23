108412594430b0d8ebdc8ead989651ccb4314d81
ViktoriyaSF <112387380+ViktoriyaSF@users.noreply.github.com>
2023-05-23 20:09:09 +0000

    Deploying to gh-pages from @ ViktoriyaSF/goit-react-hw-05-movies@e8c485cafb2a6f41319ee4cac09a362346f739e7 🚀

--- static/js/681.1f63ce3d.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[681],{681:function(e,t,r){r.r(t);var a=r(689),c=r(184);t.default=function(){var e=(0,a.UO)().movieId;return(0,c.jsxs)("div",{children:["Reviews ",e]})}}}]);
-//# sourceMappingURL=681.1f63ce3d.chunk.js.map
