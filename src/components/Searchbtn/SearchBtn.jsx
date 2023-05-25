29d7feb0856e9b7beceb40351551f1516c8dd7fb
ViktoriyaSF <kievstar01051985@gmail.com>
2023-05-26 00:17:54 +0300

    searchBtn

---
+++ src/components/Searchbtn/SearchBtn.jsx
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import { toast } from 'react-hot-toast';
+import { Form, Input, Button } from './StyledSearchBtn';
+
+const SearchMovies = ({ onSubmit }) => {
+  const handleSubmit = e => {
+    e.preventDefault(); // відміняємо стандартну поведінку браузера
+
+    const query = e.target.elements.query.value; // додаємо доступ до значення поля пошуку
+
+    // додаємо перевірку на наявність значення в полі пошуку
+    if (!query) {
+      toast.error('Please enter something');
+      return;
+    }
+
+    onSubmit(query); // додаємо функціонал для пошуку фільмів
+    e.target.reset(); // очищаємо поле пошуку
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Input name="query" type="text" placeholder="Search movies" />
+      <Button type="submit">Search</Button>
+    </Form>
+  );
+};
+
+export default SearchMovies;
+
+// const updateQueryString = evt => {
+//   const movieSearchValue = evt.target.value;
+//   if (movieSearchValue === '') {
+//     return setSearchParams({});
+//   }
+//   setSearchParams({ movieId: movieSearchValue });
+// };
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,12 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useLocation, useSearchParams } from 'react-router-dom';
+import { useLocation, useSearchParams } from 'react-router-dom';
 import { fetchSearchMovies } from 'service/api-movies';
+import SearchMovies from 'components/Searchbtn/SearchBtn';
+import { toast } from 'react-hot-toast';
+
+import {
+  StyledSection,
+  StyledLink,
+  SectionTitle,
+  List,
+  ListItem,
+} from 'components/MovieList/StyledMovieList';
 
 const Movies = () => {
   const [movies, setMovies] = useState([]);
-
   const location = useLocation();
-  // console.log(location);
 
   const [searchParams, setSearchParams] = useSearchParams();
   //або значення або пустий рядок
@@ -15,36 +23,53 @@ const Movies = () => {
   useEffect(() => {
     if (movieId === '' || movieId === null) return;
     const getMovies = async () => {
-      const { results } = await fetchSearchMovies(movieId);
-      // console.log(results);
-      setMovies(results);
+      try {
+        const { results } = await fetchSearchMovies(movieId);
+        if (results.length === 0) {
+          toast.dismiss(); // очищаємо попереднє сповіщення
+          toast.error('No movies found');
+          setMovies([]); // очищаємо масив фільмів
+        } else {
+          setMovies(results);
+        }
+      } catch (error) {
+        toast.error(error.message);
+        setMovies([]);
+      }
     };
 
     getMovies();
   }, [movieId]);
 
-  const updateQueryString = evt => {
-    const movieSearchValue = evt.target.value;
-    if (movieSearchValue === '') {
-      return setSearchParams({});
-    }
-    setSearchParams({ movieId: movieSearchValue });
+  const handleSubmit = movieId => {
+    setSearchParams({ movieId });
   };
-  // console.log(movieId);
+  // const handleSubmit = evt => {
+  //   const movieSearchValue = evt.target.value;
+  //   if (movieSearchValue === '') {
+  //     return setSearchParams({});
+  //   }
+  //   setSearchParams({ movieId: movieSearchValue });
+  // };
 
   return (
-    <div>
-      <input type="text" value={movieId} onChange={updateQueryString} />
-      {movies.map(movie => {
-        return (
-          <li key={movie.id}>
-            <Link to={`${movie.id}`} state={{ from: location }}>
-              {movie.title || movie.name}
-            </Link>
-          </li>
-        );
-      })}
-    </div>
+    <StyledSection>
+      {movies.length === 0 && (
+        <SectionTitle>Tell me what are you looking for</SectionTitle>
+      )}
+      <SearchMovies onSubmit={handleSubmit} />
+      <List>
+        {movies.map(movie => {
+          return (
+            <ListItem key={movie.id}>
+              <StyledLink to={`${movie.id}`} state={{ from: location }}>
+                {movie.title || movie.name}
+              </StyledLink>
+            </ListItem>
+          );
+        })}
+      </List>
+    </StyledSection>
   );
 };
 export default Movies;
